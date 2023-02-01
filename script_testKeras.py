0b36a3ce046cf695cc985b24efa0687f465f8c31
sapichat <sapichat@gmail.com>
2023-02-01 19:54:00 +0700

    Update script_testKeras.py

--- script_testKeras.py
+++ script_testKeras.py
@@ -1,4 +1,5 @@
-'''Trains a simple deep NN on the MNIST dataset.
+''' script was adapted from http://www.acsu.buffalo.edu/~mudin/mnist_mlp.py
+Trains a simple deep NN on the MNIST dataset.
 Gets to 98.40% test accuracy after 20 epochs
 (there is *a lot* of margin for parameter tuning).
 2 seconds per epoch on a K520 GPU.
